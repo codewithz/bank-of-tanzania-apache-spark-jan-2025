d1def83e0cf5a25e62c49d650a2e28f32a6e1001
Zartab Nakhwa <zartab@codewithz.com>
2025-01-14 12:19:40 +0300

    RDD app

--- spark-playground/src/main/java/com/codewithz/rdd/CreatingRDDFromCollection.java
+++ spark-playground/src/main/java/com/codewithz/rdd/CreatingRDDFromCollection.java
@@ -1,2 +1,39 @@
-package com.codewithz.rdd;public class CreatingRDDFromCollection {
+package com.codewithz.rdd;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class CreatingRDDFromCollection {
+
+    public static void main(String[] args) {
+
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("RDD Creation App")
+                .master("local[4]")
+                .getOrCreate();
+
+        JavaSparkContext context=JavaSparkContext.fromSparkContext(spark.sparkContext());
+
+        List<Integer> list= Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        JavaRDD<Integer> rdd=context.parallelize(list);
+
+        int numberOfPartitions=rdd.getNumPartitions();
+        System.out.println("Number of Partitions: "+numberOfPartitions);
+
+        List<Integer> output=rdd.collect();
+
+        System.out.println("Output:"+output);
+
+
+        try (final var scanner = new Scanner(System.in)) {
+            scanner.nextLine();
+        }
+    }
 }
